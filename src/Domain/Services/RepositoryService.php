9bccc553391aeb1977d2aac1471900c1fe258be1
Wilcar José Angulo S <wilcarjose@gmail.com>
2023-01-15 18:20:19 -0400

    Updated RepositoryService class

--- src/Domain/Services/RepositoryService.php
+++ src/Domain/Services/RepositoryService.php
@@ -2,19 +2,23 @@
 
 namespace Ampliffy\CiCd\Domain\Services;
 
-use Ampliffy\CiCd\Domain\Repositories\RepositoryRepositoryInterface;
+use Ampliffy\CiCd\Infrastructure\Log;
 use Ampliffy\CiCd\Domain\Dto\CommitDto;
-use Ampliffy\CiCd\Domain\Entities\Repository;
+use Ampliffy\CiCd\Domain\Dto\RepositoryDto;
 use Doctrine\Common\Collections\Collection;
+use Ampliffy\CiCd\Domain\Entities\Repository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Ampliffy\CiCd\Infrastructure\EntityManager;
+use Ampliffy\CiCd\Domain\Exceptions\ComposerException;
+use Ampliffy\CiCd\Domain\Services\ComposerJsonService;
+use Ampliffy\CiCd\Domain\Repositories\RepositoryRepositoryInterface;
 
 class RepositoryService
 {
-    public function __construct(protected RepositoryRepositoryInterface $repositoryRepository)
-    {
-        
-    }
+    public function __construct(
+        protected RepositoryRepositoryInterface $repositoryRepository,
+        protected DependencyTreeService $dependencyTreeService,
+        protected ComposerJsonService $composerJsonService
+    ) {}
 
     public function getAffectedByCommit(CommitDto $commitDto) : Collection
     {
@@ -22,30 +26,52 @@ class RepositoryService
 
         $affectedRepositories = $repositories->filter(function($repository) use ($commitDto) {
             // check which repositories are affected
-            print_r($repository->getComposerName());
-            return DependencyTreeService::hasDependency($repository, $commitDto->git_url);
+            return $this->dependencyTreeService->hasDependency($repository, $commitDto->git_url);
         });
-
-        //print_r($affectedRepositories->getValues());
 
         return $affectedRepositories;
     }
 
     public function getAll() : ArrayCollection
     {
-        $items = $this->repositoryRepository->getAll();
-        
-        if (count($items) > 0) {
-            return new ArrayCollection($items);
-        }
+        $directories = $this->getDirectoriesNamesFromBasePath();
 
-        return $this->repositoryRepository->createRepositories();
+        $directories->map(function ($folder) {
+
+            try {
+
+                if ($this->repositoryRepository->doesNotExistByGitPath($folder)) {
+                    $composerData = $this->composerJsonService->getComposerContent($folder);
+                    $this->repositoryRepository->createRepository(RepositoryDto::fromComposerJson($composerData, $folder));
+                }
+
+            } catch (ComposerException $e) {
+                Log::debug($e->getMessage());
+            }
+
+        });
+
+        return $this->repositoryRepository->getAll();
     }
 
-    public function getByGitPath(string $gitPath) : Repository
+    public function getByGitPath(string $gitPath) : Repository|null
     {
         return $this->repositoryRepository->getByGitPath($gitPath);
     }
 
-    
+    protected function getDirectoriesNamesFromBasePath() : ArrayCollection
+    {
+        $directories = scandir($_ENV['BASE_PATH']);
+
+        if ($directories === false) {
+            return new ArrayCollection();
+        }
+
+        return (new ArrayCollection($directories))->filter(fn ($dir) => $this->isDirectory($dir));
+    }
+
+    protected function isDirectory($name) : bool
+    {
+        return is_dir($_ENV['BASE_PATH'] . $name);
+    }
 }
